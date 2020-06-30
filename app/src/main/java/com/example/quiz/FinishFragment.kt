5a08bf4638d409a76d87e752698561cd8285abf1
Eciric <40577400+Eciric@users.noreply.github.com>
2020-06-30 23:15:46 +0200

    FinishFragment update

--- app/src/main/java/com/example/quiz/FinishFragment.kt
+++ app/src/main/java/com/example/quiz/FinishFragment.kt
@@ -5,14 +5,49 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_finish.*
+import kotlinx.android.synthetic.main.fragment_login.*
 
-class FinishFragment : Fragment() {
+class FinishFragment : Fragment(), View.OnClickListener {
+
+    lateinit var navController: NavController
+    lateinit var login: String
+    lateinit var score: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        login = requireArguments().getString("login").toString()
+        score = requireArguments().getString("score").toString()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_finish, container, false)
+        return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        buttonRestart.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id) {
+            R.id.buttonStart -> {
+                login = et_name.text.toString()
+                if (login.isNotEmpty()) {
+                    val bundle = bundleOf("login" to login)
+                    navController.navigate(R.id.action_loginFragment_to_questionsFragment, bundle)
+                } else {
+                    Toast.makeText(activity, "Enter the login", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/quiz/QuestionsFragment.kt
+++ app/src/main/java/com/example/quiz/QuestionsFragment.kt
@@ -5,9 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_login.*
 
 
 class QuestionsFragment : Fragment() {
+
+    lateinit var navController: NavController
+    lateinit var login: String
+    lateinit var score: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        login = requireArguments().getString("login").toString()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,5 +27,10 @@ class QuestionsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_questions, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
     }
 }
